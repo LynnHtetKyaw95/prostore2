0222ac3339991cff6514538e7b31ba9b3478bf40
lhk <dklin1995@gmail.com>
2026-03-27 21:52:26 +0700

    dark mode toggle

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { DarkModeToggle } from "./DarkModeToggle";
 
 const Header = () => {
   return (
@@ -24,13 +25,14 @@ const Header = () => {
         </div>
 
         <div className="space-x-2">
-          <Button variant="ghost">
-            <Link href="/cart" className="flex-center gap-2">
+          <DarkModeToggle />
+          <Button variant="ghost" asChild>
+            <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button>
-            <Link href="/sign-in" className="flex-center gap-2">
+          <Button asChild>
+            <Link href="/sign-in">
               <UserIcon /> Sign In
             </Link>
           </Button>

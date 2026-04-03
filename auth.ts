dbeeb2cd81ce29cc89954f4ade8af9c52a184f2c
lhk <dklin1995@gmail.com>
2026-04-03 10:02:42 +0700

    add middleware and make sessionCartID

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -92,6 +93,30 @@ export const config = {
       }
 
       return token;
+    },
+    authorized({ request, auth }: any) {
+      // Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new reponse and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set newly generated sessionCartID in the response cookie
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
